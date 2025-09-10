edc376f49f01e594ca4948838c35b005df9b5242
Rachel <dcostarachel3@gmail.com>
2025-09-10 16:37:47 +1200

    debug: log mongo uri

--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ import cors from "cors";
 import dotenv from "dotenv";
 
 dotenv.config();
+console.log("MONGO_URI value:", process.env.MONGO_URI); 
 const app = express();
 app.use(cors());
 app.use(express.json());
